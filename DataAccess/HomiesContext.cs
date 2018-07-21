479583a1fcad912a10560c457be68699f2f07b01
loganFry <fry.348@osu.com>
2018-07-21 11:35:52 -0400

    Organize database access files

---
+++ DataAccess/HomiesContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace HomiesAPI
+{
+    public partial class HomiesContext : DbContext
+    {
+        public HomiesContext()
+        {
+        }
+
+        public HomiesContext(DbContextOptions<HomiesContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Homies> Homies { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("Host=localhost;Database=Homies;Username=postgres;Password=lF699132");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Homies>(entity =>
+            {
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Nickname)
+                    .IsRequired()
+                    .HasMaxLength(100);
+            });
+        }
+    }
+}
