96cfffe89f7b8194fa85d95dcd4a476d0d8716c2
Logan Fry <loganfrybball@yahoo.com>
2018-07-21 12:10:19 -0400

    Add controller for homies

---
+++ Controllers/HomiesController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using HomiesAPI.DataAccess;
+using HomiesAPI.Models;
+
+namespace HomiesAPI.Controllers
+{    
+    [Route("[controller]")]
+    [ApiController]
+    public class HomiesController : ControllerBase
+    {
+        private HomiesContext _context;
+
+        public HomiesController(HomiesContext context)
+        {
+            _context = context;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string[]> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
