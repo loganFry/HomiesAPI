0e7b972ebe02dca999eb440e02f59c757f3cb00e
loganFry <fry.348@osu.com>
2018-07-21 11:32:56 -0400

    Initial commit, hook up app to local db

---
+++ Homies.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomiesAPI
+{
+    public partial class Homies
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+    }
+}
