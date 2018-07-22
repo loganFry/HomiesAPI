e5fe0b4ac5ab493e4634f3418b48289d37354860
Logan Fry <loganfrybball@yahoo.com>
2018-07-22 13:30:50 -0400

    Delete sample controller methods

--- Controllers/HomiesController.cs
+++ Controllers/HomiesController.cs
@@ -19,36 +19,12 @@ namespace HomiesAPI.Controllers
             _context = context;
         }
 
-        // GET api/values
         [HttpGet]
         public ActionResult<IEnumerable<Homies>> Get()
         {
             return _context.Homies;
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+        
     }
 }
