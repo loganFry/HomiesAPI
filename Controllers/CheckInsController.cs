87fb1d9f5e35cae008397b63fe1a3380a3b14625
loganFry <fry.348@osu.com>
2018-07-31 14:32:41 -0400

    Finishing get/create methods for checkins

--- Controllers/CheckInsController.cs
+++ Controllers/CheckInsController.cs
@@ -25,6 +25,27 @@ namespace HomiesAPI.Controllers
             return _context.CheckIns.ToList();
         }
 
+        [HttpGet("checkins/{id}", Name="GetCheckInById")]
+        public ActionResult<CheckIn> GetById(int id)
+        {
+            var checkIn = _context.CheckIns.FirstOrDefault(x => x.Id == id);
+            if(checkIn == default(CheckIn))
+            {
+                return NotFound();
+            }
+
+            return checkIn;
+        }
+
+        [HttpPost("checkins")]
+        public IActionResult Create([FromBody] CheckIn checkIn)
+        {
+            _context.CheckIns.Add(checkIn);
+            _context.SaveChanges();
+
+            return CreatedAtRoute("GetCheckInById", new { id = checkIn.Id }, checkIn);
+        }
+
         [HttpGet("homies/{homieId}/checkins")]
         public ActionResult<IEnumerable<CheckIn>> GetAllForHomie(int homieId)
         {
@@ -36,6 +57,25 @@ namespace HomiesAPI.Controllers
             _context.Entry(homie).Collection(x => x.CheckIns).Load();
 
             return homie.CheckIns.ToList();
+        }
+
+        [HttpGet("homies/{homieId}/checkins/{id}", Name="GetHomieCheckInById")]
+        public ActionResult<CheckIn> GetByIdForHomie(int homieId, int id)
+        {
+            var homie = _context.Homies.FirstOrDefault(x => x.Id == homieId);
+            if(homie == default(Homie))
+            {
+                return NotFound();
+            }
+            _context.Entry(homie).Collection(x => x.CheckIns).Load();
+
+            var checkIn = homie.CheckIns.FirstOrDefault(x => x.Id == id);
+            if(checkIn == default(CheckIn))
+            {
+                return NotFound();
+            }
+
+            return checkIn;
         }
     }
 }
