efd1f380b346270be5441f93a1949a13577c1397
Logan Fry <loganfrybball@yahoo.com>
2018-08-02 16:55:45 -0400

    Add support for loading related entities

--- DataAccess/Repositories/IRepository.cs
+++ DataAccess/Repositories/IRepository.cs
@@ -6,9 +6,10 @@ namespace HomiesAPI.DataAccess.Repositories
 {
     public interface IRepository<T> where T : EntityBase
     {
-        T GetById(int id);
-        IEnumerable<T> List();
-        IEnumerable<T> List(Expression<Func<T, bool>> predicate);
+        T GetById(int id, Expression<Func<T, object>>[] includes = null);
+        T Get(Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null);
+        IEnumerable<T> List(Expression<Func<T, object>>[] includes = null);
+        IEnumerable<T> List(Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null);
         void Add(T entity);
         void Delete(T entity);
         void Edit(T entity);
--- DataAccess/Repositories/Repository.cs
+++ DataAccess/Repositories/Repository.cs
@@ -4,6 +4,7 @@ using HomiesAPI.Models;
 using System.Data;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace HomiesAPI.DataAccess.Repositories
 {
@@ -11,26 +12,54 @@ namespace HomiesAPI.DataAccess.Repositories
     {
         protected readonly HomiesContext _dbContext;
 
+        protected DbSet<T> _dbSet;
+
         public Repository(HomiesContext dbContext)
         {
             _dbContext = dbContext;
+            _dbSet = _dbContext.Set<T>();
         }
 
-        public virtual T GetById(int id)
+        public virtual T GetById(int id, Expression<Func<T, object>>[] includes = null)
         {
-            return _dbContext.Set<T>().Find(id);
+            return Get(T => T.Id == id, includes);
         }
 
-        public virtual IEnumerable<T> List()
+        public virtual T Get(Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null)
         {
-            return _dbContext.Set<T>();
+            IQueryable<T> query = _dbSet;
+            if (includes != null)
+            {
+                foreach (var include in includes)
+                {
+                    query.Include(include);
+                }
+            }
+
+            return query.FirstOrDefault(predicate);
         }
 
-        public virtual IEnumerable<T> List(System.Linq.Expressions.Expression<Func<T, bool>> predicate)
+        public virtual IEnumerable<T> List(Expression<Func<T, object>>[] includes = null)
         {
-            return _dbContext.Set<T>()
-                   .Where(predicate)
-                   .AsEnumerable();
+            var elements = _dbContext.Set<T>();
+            if (includes != null)
+            {
+                return includes.Aggregate(elements, (current, include) => (DbSet<T>)current.Include(include));
+            }
+
+            return elements;
+        }
+
+        public virtual IEnumerable<T> List(Expression<Func<T, bool>> predicate, Expression<Func<T, object>>[] includes = null)
+        {
+            var elements = _dbContext.Set<T>().Where(predicate);
+            if (includes != null)
+            {
+                return includes
+                    .Aggregate(elements, (current, include) => (DbSet<T>)current.Include(include))
+                    .Where(predicate);
+            }
+            return elements;
         }
 
         public void Add(T entity)
