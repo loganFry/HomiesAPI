40586aba1d1571aff6fa7e07056a622abfc9f0ee
Logan Fry <loganfrybball@yahoo.com>
2018-08-03 12:43:54 -0400

    Finish basic methods for check out controller

--- Controllers/CheckOutsController.cs
+++ Controllers/CheckOutsController.cs
@@ -3,33 +3,79 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using HomiesAPI.DataAccess;
 using HomiesAPI.Models;
-using Microsoft.EntityFrameworkCore;
+using HomiesAPI.DataAccess.Repositories;
+using System.Linq.Expressions;
 
 namespace HomiesAPI.Controllers
 {
-    [Route("api/homies")]
+    [Route("api")]
     public class CheckOutsController : ControllerBase
     {
-        private HomiesContext _context;
+        private ICheckOutRepository _checkOutRepo;
 
-        public CheckOutsController(HomiesContext context)
+        private IHomieRepository _homieRepo;
+
+        public CheckOutsController(ICheckOutRepository checkOutRepo, IHomieRepository homieRepo)
         {
-            _context = context;
+            _checkOutRepo = checkOutRepo;
+            _homieRepo = homieRepo;
         }
 
-        [HttpGet("{homieId}/checkouts")]
+        [HttpGet("checkouts")]
         public ActionResult<IEnumerable<CheckOut>> GetAll(int homieId)
         {
-            var homie = _context.Homies.FirstOrDefault(x => x.Id == homieId);
-            if(homie == default(Homie))
+            var checkOuts = _checkOutRepo.List().ToList();
+
+            return checkOuts;
+        }
+
+        [HttpGet("checkouts/{id}", Name="GetCheckOutById")]
+        public ActionResult<CheckOut> GetById(int id)
+        {
+            var checkOut = _checkOutRepo.GetById(id);
+            if(checkOut == null)
             {
                 return NotFound();
             }
-            _context.Entry(homie).Collection(x => x.CheckOuts).Load();
+
+            return checkOut;
+        }
+
+        [HttpGet("homies/{homieId}/checkouts")]
+        public ActionResult<IEnumerable<CheckOut>> GetAllForHomie(int homieId)
+        {
+            var includes = new Expression<Func<Homie, object>>[] {
+                x => x.CheckOuts
+            };
+            var homie = _homieRepo.GetById(homieId, includes);
+            if(homie == null)
+            {
+                return NotFound();
+            }
 
             return homie.CheckOuts.ToList();
+        }
+
+        [HttpGet("homies/{homieId}/checkouts/{id}", Name="GetHomieCheckOutById")]
+        public ActionResult<CheckOut> GetByIdForHomie(int homieId, int id)
+        {
+            var includes = new Expression<Func<Homie, object>>[] {
+                x => x.CheckOuts
+            };
+            var homie = _homieRepo.GetById(homieId, includes);
+            if(homie == null)
+            {
+                return NotFound();
+            }
+
+            var checkIn = homie.CheckOuts.FirstOrDefault(x => x.Id == id);
+            if(checkIn == null)
+            {
+                return NotFound();
+            }
+
+            return checkIn;
         }
     }
 }
